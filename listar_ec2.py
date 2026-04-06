43ce37177ce410d77893cf38e62962c5919438fb
Santos Jose <jose.xzx.santos@scania.com>
2026-04-06 13:32:04 -0300

    fix region no codigo

--- listar_ec2.py
+++ listar_ec2.py
@@ -4,9 +4,6 @@ ec2 = boto3.client('ec2', region_name='us-east-1')
 
 response = ec2.describe_instances()
 
-for reservation in response['Reservations']:
-    for instance in reservation['Instances']:
-        print("ID:", instance['InstanceId'])
-        print("Estado:", instance['State']['Name'])
-        print("Tipo:", instance['InstanceType'])
-        print("-" * 30)
+for r in response['Reservations']:
+    for i in r['Instances']:
+        print(i['InstanceId'], i['State']['Name'])
